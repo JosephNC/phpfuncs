ca1d3c02728be8cf6f49bf00dc4228c1d2240522
JosephNC <chukwudinwakpaka@hotmail.com>
2018-05-16 01:06:27 +0100

    Added new functions and removed composer.lock file from history.

--- src/phpfuncs.php
+++ src/phpfuncs.php
@@ -152,3 +152,319 @@ function get_random_string( $length = 20 )
 
     return $s;
 }
+
+/**
+* Returns Supported currencies
+* @param string $currency Optional currency data to retrieve.
+* @return object
+*/
+function currencies($currency = null)
+{
+    /**
+    * List of support currencies for
+    * 1 - This type of currency does not support decimals.
+    *
+    * @var object
+    */
+    $currencies = json_decode(json_encode([
+        'AUD' => [
+            'name'          => 'Australian Dollar',
+            'major'         => 'dollar',
+            'minor'         => 'cent',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ' ',
+        ],
+
+        'BRL' => [
+            'name'          => 'Brazilian Real',
+            'major'         => 'real',
+            'minor'         => 'centavo',
+            'symbol'        => 'R$',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => '.',
+        ],
+
+        'CAD' => [
+            'name'          => 'Canadian Dollar',
+            'major'         => 'dollar',
+            'minor'         => 'cent',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'CNY' => [
+            'name'          => 'Chinese Yuan Renminbi',
+            'major'         => 'yuan renminbi',
+            'minor'         => 'jiao',
+            'symbol'        => '¥',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'CZK' => [
+            'name'          => 'Czech Koruna',
+            'major'         => 'koruna',
+            'minor'         => 'haler',
+            'symbol'        => 'Kč',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => '.',
+        ],
+
+        'DKK' => [
+            'name'          => 'Danish Krone',
+            'major'         => 'krone',
+            'minor'         => 'øre',
+            'symbol'        => 'kr',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => '.',
+        ],
+
+        'EUR' => [
+            'name'          => 'Euro',
+            'major'         => 'euro',
+            'minor'         => 'cent',
+            'symbol'        => '€',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'HKD' => [
+            'name'          => 'Hong Kong Dollar',
+            'major'         => 'dollar',
+            'minor'         => 'cent',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'HUF' => [
+            'name'          => 'Hungarian Forint',
+            'major'         => 'forint',
+            'minor'         => '',
+            'symbol'        => 'Ft',
+            'decimals'      => 0,
+            'decimal_sep'   => '',
+            'thousand_sep'  => '.',
+        ], // 1
+
+        'ILS' => [
+            'name'          => 'Israeli New Shekel',
+            'major'         => 'new shekel',
+            'minor'         => 'agora',
+            'symbol'        => '₪',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'JPY' => [
+            'name'          => 'Japanese Yen',
+            'major'         => 'yen',
+            'minor'         => 'sen',
+            'symbol'        => '¥',
+            'decimals'      => 0,
+            'decimal_sep'   => '',
+            'thousand_sep'  => ',',
+        ], // 1
+
+        'MYR' => [
+            'name'          => 'Malaysian Ringgit',
+            'major'         => 'ringgit',
+            'minor'         => 'sen',
+            'symbol'        => 'RM',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'MXN' => [
+            'name'          => 'Mexican Peso',
+            'major'         => 'peso',
+            'minor'         => 'centavo',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'NGN' => [
+            'name'          => 'Nigerian Naira',
+            'major'         => 'naira',
+            'minor'         => 'kobo',
+            'symbol'        => '₦',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'NZD' => [
+            'name'          => 'New Zealand Dollar',
+            'major'         => 'dollar',
+            'minor'         => 'cent',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'NOK' => [
+            'name'          => 'Norwegian Krone',
+            'major'         => 'krone',
+            'minor'         => 'øre',
+            'symbol'        => 'kr',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => '.',
+        ],
+
+        'PHP' => [
+            'name'          => 'Philippine Peso',
+            'major'         => 'peso',
+            'minor'         => 'centavo',
+            'symbol'        => '₱',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'PLN' => [
+            'name'          => 'Polish Zloty',
+            'major'         => 'zloty',
+            'minor'         => 'grosz',
+            'symbol'        => 'zł',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => ' ',
+        ],
+
+        'GBP' => [
+            'name'          => 'Pound Sterling',
+            'major'         => 'pound',
+            'minor'         => 'pence',
+            'symbol'        => '£',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'RUB' => [
+            'name'          => 'Russian Ruble',
+            'major'         => 'ruble',
+            'minor'         => 'kopeck',
+            'symbol'        => '₽',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => '.',
+        ],
+
+        'SGD' => [
+            'name'          => 'Singapore Dollar',
+            'major'         => 'dollar',
+            'minor'         => 'cent',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'SEK' => [
+            'name'          => 'Swedish Krona',
+            'major'         => 'krona',
+            'minor'         => 'öre',
+            'symbol'        => 'kr',
+            'decimals'      => 2,
+            'decimal_sep'   => ',',
+            'thousand_sep'  => ' ',
+        ],
+
+        'CHF' => [
+            'name'          => 'Swiss Franc',
+            'major'         => 'franken',
+            'minor'         => 'rappen',
+            'symbol'        => 'CHF',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => "'",
+        ],
+
+        'TWD' => [
+            'name'          => 'Taiwan New Dollar',
+            'major'         => 'new dollar',
+            'minor'         => 'cent',
+            'symbol'        => 'NT$',
+            'decimals'      => 0,
+            'decimal_sep'   => '',
+            'thousand_sep'  => ',',
+        ], // 1
+
+        'THB' => [
+            'name'          => 'Thai baht',
+            'major'         => 'baht',
+            'minor'         => 'satang',
+            'symbol'        => '฿',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+
+        'USD' => [
+            'name'          => 'United States Dollar',
+            'major'         => 'dollar',
+            'minor'         => 'cent',
+            'symbol'        => '$',
+            'decimals'      => 2,
+            'decimal_sep'   => '.',
+            'thousand_sep'  => ',',
+        ],
+    ]));
+
+    if (! empty($currency) && isset($currencies->$currency)) {
+        return $currencies->$currency;
+    }
+
+    return $currencies;
+}
+
+/**
+* Formats a number as a currency string
+*
+* @param int|float(double) $number The number to be formatted
+* @param string $code Currency to use(Alpha ISO 4217 Code) , will be replaced with symbol if symbol is true. Defaults to usd
+* @param bool $symbol Whether to use the currency symbol or not
+* @param string $position Where to place the currency/symbol. Accepts 'left', 'right', 'left_space', 'right_space'. Default 'left_space'
+* @return string Returns the formatted string or false if $code is not a string
+*/
+function money_form($number, $code = 'usd', $symbol = false, $position = 'left_space')
+{
+    $code       = empty($code) ? 'usd' : $code;
+    $position   = empty($position) ? 'left_space' : $position;
+
+    $currency   = currencies(strtoupper($code));
+    $number     = empty($number) ? 0.00 : floatval($number);
+    $symbol     = $symbol ? $currency->symbol : $code;
+
+    $decimals       = $currency->decimals;
+    $decimal_sep    = $currency->decimal_sep;
+    $thousand_sep   = $currency->thousand_sep;
+    $number         = number_format($number, $decimals, $decimal_sep, $thousand_sep);
+
+    switch ($position) {
+        case 'left' :           $money = "$symbol$number"; break;
+        case 'left_space' :     $money = "$symbol $number"; break;
+        case 'right' :          $money = "$number$symbol"; break;
+        case 'right_space' :    $money = "$number $symbol"; break;
+        default :               $money = "$symbol$number"; break;
+    }
+
+    return $money;
+}
